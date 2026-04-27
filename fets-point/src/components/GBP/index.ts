72f4ce31195e707305a082e660236861dee690f8
hy4k <mithun@fets.in>
2026-04-28 02:27:36 +0530

    feat: add GBP components barrel export index.ts

---
+++ fets-point/src/components/GBP/index.ts
@@ -0,0 +1,4 @@
+export { default as GBPReviewPanel } from './GBPReviewPanel';
+export { default as GBPInsightsPanel } from './GBPInsightsPanel';
+export { default as GBPPostPublisher } from './GBPPostPublisher';
+export { default as GBPQandA } from './GBPQandA';
